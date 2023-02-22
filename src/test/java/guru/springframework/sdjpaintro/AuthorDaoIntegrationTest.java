568a2388c7308bd081d65aacbff9ad7b59647455
yeahbutstill <mayadani28@gmail.com>
2023-02-22 18:41:02 +0700

    update test for book

--- src/test/java/guru/springframework/sdjpaintro/AuthorDaoIntegrationTest.java
+++ src/test/java/guru/springframework/sdjpaintro/AuthorDaoIntegrationTest.java
@@ -3,6 +3,7 @@ package guru.springframework.sdjpaintro;
 import guru.springframework.sdjpaintro.dao.AuthorDao;
 import guru.springframework.sdjpaintro.dao.BookDao;
 import guru.springframework.sdjpaintro.domain.Author;
+import guru.springframework.sdjpaintro.domain.Book;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -81,6 +82,72 @@ class AuthorDaoIntegrationTest {
 
         Author deleted = authorDao.getById(saved.getId());
         Assertions.assertThat(deleted).isNull();
+
+   }
+
+   @Test
+    void testDeleteBook() {
+
+        Book book = new Book();
+        book.setIsbn("4321");
+        book.setPublisher("Self");
+        book.setTitle("My Book");
+        book.setAuthorId(3L);
+
+        Book saved = bookDao.saveNewBook(book);
+        bookDao.deleteBookById(saved.getId());
+
+        Book deleted = bookDao.getById(saved.getId());
+        Assertions.assertThat(deleted).isNull();
+
+   }
+
+   @Test
+    void updateBookTest() {
+
+       Book book = new Book();
+       book.setIsbn("4321");
+       book.setPublisher("Self");
+       book.setTitle("My Book");
+       book.setAuthorId(3L);
+
+       Book saved = bookDao.saveNewBook(book);
+       saved.setTitle("New Book");
+
+       bookDao.updateBook(saved);
+
+       Book fetched = bookDao.getById(saved.getId());
+       Assertions.assertThat(fetched.getTitle()).isEqualTo("New Book");
+
+   }
+
+   @Test
+    void testSaveBook() {
+
+        Book book = new Book();
+        book.setIsbn("12345");
+        book.setPublisher("Self");
+        book.setTitle("my book");
+        book.setAuthorId(3L);
+
+        Book saved = bookDao.saveNewBook(book);
+        Assertions.assertThat(saved).isNotNull();
+
+   }
+
+   @Test
+    void testGetBookByName() {
+
+        Book book = bookDao.findBookByTitle("Clean Code");
+        Assertions.assertThat(book).isNotNull();
+
+   }
+
+   @Test
+    void testGetBookById() {
+
+        Book book = bookDao.getById(3L);
+        Assertions.assertThat(book.getId()).isNotNull();
 
    }
 
