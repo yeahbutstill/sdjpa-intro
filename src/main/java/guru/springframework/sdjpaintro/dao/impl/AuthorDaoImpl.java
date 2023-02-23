7d6ef58e8c8f99a5228f125872ad974a7a8e36f6
yeahbutstill <mayadani28@gmail.com>
2023-02-23 15:28:16 +0700

    implement get by id

--- src/main/java/guru/springframework/sdjpaintro/dao/impl/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/sdjpaintro/dao/impl/AuthorDaoImpl.java
@@ -5,10 +5,9 @@ import guru.springframework.sdjpaintro.domain.Author;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
-
-import javax.sql.DataSource;
-import java.sql.*;
 
 @Component
 public class AuthorDaoImpl implements AuthorDao {
@@ -16,210 +15,39 @@ public class AuthorDaoImpl implements AuthorDao {
     private final Logger logger = LoggerFactory.getLogger(AuthorDaoImpl.class);
     private static final String CONTEXT_AUTHOR = "context author";
 
-    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
 
     @Autowired
-    public AuthorDaoImpl(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public AuthorDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public Author getById(Long id) {
-
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-
-        try {
-
-            connection = dataSource.getConnection();
-            preparedStatement = connection.prepareStatement("SELECT  * FROM author WHERE id = ?");
-            preparedStatement.setLong(1, id);
-
-            // This not best practice
-            //resultSet = statement.executeQuery("SELECT * FROM author WHERE id = " + id);
-
-            // Do this
-            resultSet = preparedStatement.executeQuery();
-
-            if (resultSet.next()) {
-                return getAuthorFromResultSet(resultSet);
-
-            }
-        } catch (SQLException e) {
-            logger.info(CONTEXT_AUTHOR, e);
-        } finally {
-            try {
-                closeAll(resultSet, preparedStatement, connection);
-            } catch (SQLException e) {
-                logger.info(CONTEXT_AUTHOR, e);
-            }
-        }
-
-        return null;
-
+        return jdbcTemplate.queryForObject("SELECT * FROM author WHERE id = ?", getRowMapper(), id);
     }
 
     @Override
     public Author findAuthorByName(String firstName, String lastName) {
-
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-
-        try {
-
-            connection = dataSource.getConnection();
-            preparedStatement = connection.prepareStatement("SELECT * FROM author WHERE first_name = ? AND last_name = ?");
-            preparedStatement.setString(1, firstName);
-            preparedStatement.setString(2, lastName);
-            resultSet = preparedStatement.executeQuery();
-
-            if (resultSet.next()) {
-                return getAuthorFromResultSet(resultSet);
-            }
-
-        } catch (SQLException e) {
-            logger.info(CONTEXT_AUTHOR, e);
-        } finally {
-
-            try {
-                closeAll(resultSet, preparedStatement, connection);
-            } catch (SQLException e) {
-                logger.info(CONTEXT_AUTHOR, e);
-            }
-
-        }
-
         return null;
-
     }
 
     @Override
     public Author saveNewAuthor(Author author) {
-
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        Statement statement = null;
-
-        try {
-
-            connection = dataSource.getConnection();
-            preparedStatement = connection.prepareStatement("INSERT INTO author (first_name, last_name) VALUES (?, ?) RETURNING id");
-            preparedStatement.setString(1, author.getFirstName());
-            preparedStatement.setString(2, author.getLastName());
-            preparedStatement.execute();
-
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT LASTVAL()");
-
-            if (resultSet.next()) {
-                Long savedId = resultSet.getLong(1);
-                return this.getById(savedId);
-            }
-
-        } catch (SQLException e) {
-            logger.info(CONTEXT_AUTHOR, e);
-        } finally {
-            try {
-                assert statement != null;
-                statement.close();
-            } catch (SQLException e) {
-                logger.info(CONTEXT_AUTHOR, e);
-            }
-            try {
-                closeAll(resultSet, preparedStatement, connection);
-            } catch (SQLException e) {
-                logger.info(CONTEXT_AUTHOR, e);
-            }
-
-        }
-
         return null;
-
     }
 
     @Override
-    public Author updateAuthor(Author author) {
-
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-
-
-        try {
-
-            connection = dataSource.getConnection();
-            preparedStatement = connection.prepareStatement("UPDATE author SET first_name = ?, last_name = ? WHERE author.id = ?");
-            preparedStatement.setString(1, author.getFirstName());
-            preparedStatement.setString(2, author.getLastName());
-            preparedStatement.setLong(3, author.getId());
-            preparedStatement.execute();
-
-        } catch (SQLException e) {
-            logger.info(CONTEXT_AUTHOR, e);
-        } finally {
-
-            try {
-                closeAll(null, preparedStatement, connection);
-            } catch (SQLException e) {
-                logger.info(CONTEXT_AUTHOR, e);
-            }
-
-        }
-
-        return this.getById(author.getId());
-
+    public Author updateAuthor(Author saved) {
+        return null;
     }
 
     @Override
     public void deleteAuthorById(Long id) {
 
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = connection.prepareStatement("DELETE FROM author WHERE id = ?");
-            preparedStatement.setLong(1, id);
-            preparedStatement.execute();
-        } catch (SQLException e) {
-            logger.info(CONTEXT_AUTHOR, e);
-        } finally {
-            try {
-                closeAll(null, preparedStatement, connection);
-            } catch (SQLException e) {
-                logger.info(CONTEXT_AUTHOR, e);
-            }
-        }
-
     }
 
-    private void closeAll(ResultSet resultSet, PreparedStatement preparedStatement, Connection connection) throws SQLException {
-
-        if (resultSet != null) {
-            resultSet.close();
-        }
-
-        if (preparedStatement != null) {
-            preparedStatement.close();
-        }
-
-        if (connection != null) {
-            connection.close();
-        }
-
+    private RowMapper<Author> getRowMapper() {
+        return new AuthorMapper();
     }
-
-    private Author getAuthorFromResultSet(ResultSet resultSet) throws SQLException {
-
-        Author author = new Author();
-        author.setId(resultSet.getLong("id"));
-        author.setFirstName(resultSet.getString("first_name"));
-        author.setLastName(resultSet.getString("last_name"));
-
-        return author;
-
-    }
-
 }
--- src/main/java/guru/springframework/sdjpaintro/dao/impl/BookDaoImpl.java
+++ src/main/java/guru/springframework/sdjpaintro/dao/impl/BookDaoImpl.java
@@ -1,14 +1,10 @@
 package guru.springframework.sdjpaintro.dao.impl;
 
-import guru.springframework.sdjpaintro.dao.AuthorDao;
 import guru.springframework.sdjpaintro.dao.BookDao;
 import guru.springframework.sdjpaintro.domain.Book;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
-import javax.sql.DataSource;
-import java.sql.*;
 
 @Component
 public class BookDaoImpl implements BookDao {
@@ -16,196 +12,30 @@ public class BookDaoImpl implements BookDao {
     private final Logger logger = LoggerFactory.getLogger(BookDaoImpl.class);
     private static final String CONTEXT_BOOK = "context book";
 
-    private final DataSource source;
-    private final AuthorDao authorDao;
-
-    public BookDaoImpl(DataSource source, AuthorDao authorDao) {
-        this.source = source;
-        this.authorDao = authorDao;
-    }
 
     @Override
     public Book getById(Long id) {
-        Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet resultSet = null;
-
-        try {
-            connection = source.getConnection();
-            ps = connection.prepareStatement("SELECT * FROM book where id = ?");
-            ps.setLong(1, id);
-            resultSet = ps.executeQuery();
-
-            if (resultSet.next()) {
-                return getBookFromRS(resultSet);
-            }
-        } catch (SQLException e) {
-            logger.info(CONTEXT_BOOK, e);
-        } finally {
-            try {
-                closeAll(resultSet, ps, connection);
-
-            } catch (SQLException e) {
-                logger.info(CONTEXT_BOOK, e);
-            }
-        }
-
         return null;
     }
 
     @Override
     public Book findBookByTitle(String title) {
-        Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet resultSet = null;
-
-        try {
-            connection = source.getConnection();
-            ps = connection.prepareStatement("SELECT * FROM book where title = ?");
-            ps.setString(1, title);
-            resultSet = ps.executeQuery();
-
-            if (resultSet.next()) {
-                return getBookFromRS(resultSet);
-            }
-        } catch (SQLException e) {
-            logger.info(CONTEXT_BOOK, e);
-        } finally {
-            try {
-                closeAll(resultSet, ps, connection);
-            } catch (SQLException e) {
-                logger.info(CONTEXT_BOOK, e);
-            }
-        }
-
         return null;
     }
 
     @Override
     public Book saveNewBook(Book book) {
-        Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet resultSet = null;
-
-        Statement statement = null;
-        try {
-            connection = source.getConnection();
-            ps = connection.prepareStatement("INSERT INTO book (isbn, publisher, title, author_id) VALUES (?, ?, ?, ?) RETURNING id");
-            ps.setString(1, book.getIsbn());
-            ps.setString(2, book.getPublisher());
-            ps.setString(3, book.getTitle());
-
-            if (book.getAuthor() != null) {
-                ps.setLong(4, book.getAuthor().getId());
-            } else {
-                ps.setNull(4, -5);
-            }
-
-            ps.execute();
-
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT LASTVAL()");
-
-            if (resultSet.next()) {
-                Long savedId = resultSet.getLong(1);
-                return this.getById(savedId);
-            }
-
-        } catch (SQLException e) {
-            logger.info(CONTEXT_BOOK, e);
-        } finally {
-            try {
-                assert statement != null;
-                statement.close();
-            } catch (SQLException e) {
-                logger.info(CONTEXT_BOOK, e);
-            }
-            try {
-                closeAll(resultSet, ps, connection);
-            } catch (SQLException e) {
-                logger.info(CONTEXT_BOOK, e);
-            }
-        }
-
         return null;
     }
 
     @Override
     public Book updateBook(Book book) {
-        Connection connection = null;
-        PreparedStatement ps = null;
-
-
-        try {
-            connection = source.getConnection();
-            ps = connection.prepareStatement("UPDATE book set isbn = ?, publisher = ?, title = ?, author_id = ? where id = ?");
-            ps.setString(1, book.getIsbn());
-            ps.setString(2, book.getPublisher());
-            ps.setString(3, book.getTitle());
-
-            if (book.getAuthor() != null) {
-                ps.setLong(4, book.getAuthor().getId());
-            }
-
-            ps.setLong(5, book.getId());
-            ps.execute();
-
-        } catch (SQLException e) {
-            logger.info(CONTEXT_BOOK, e);
-        } finally {
-            try {
-                closeAll(null, ps, connection);
-            } catch (SQLException e) {
-                logger.info(CONTEXT_BOOK, e);
-            }
-        }
-
-        return getById(book.getId());
+        return null;
     }
 
     @Override
     public void deleteBookById(Long id) {
-        Connection connection = null;
-        PreparedStatement ps = null;
 
-        try {
-            connection = source.getConnection();
-            ps = connection.prepareStatement("DELETE from book where id = ?");
-            ps.setLong(1, id);
-            ps.execute();
-        } catch (SQLException e) {
-            logger.info(CONTEXT_BOOK, e);
-        } finally {
-            try {
-                closeAll(null, ps, connection);
-            } catch (SQLException e) {
-                logger.info(CONTEXT_BOOK, e);
-            }
-        }
     }
 
-    private Book getBookFromRS(ResultSet resultSet) throws SQLException {
-        Book book = new Book();
-        book.setId(resultSet.getLong(1));
-        book.setIsbn(resultSet.getString(2));
-        book.setPublisher(resultSet.getString(3));
-        book.setTitle(resultSet.getString(4));
-        book.setAuthor(authorDao.getById(resultSet.getLong(5)));
-
-        return book;
-    }
-
-    private void closeAll(ResultSet resultSet, PreparedStatement ps, Connection connection) throws SQLException {
-        if (resultSet != null) {
-            resultSet.close();
-        }
-
-        if (ps != null){
-            ps.close();
-        }
-
-        if (connection != null){
-            connection.close();
-        }
-    }
 }
